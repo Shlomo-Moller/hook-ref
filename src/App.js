5f66d1bbb92c8824b8092d6b57cbfa54ef32fd06
Shlomo Moller <shlomo.moller@tandemg.com>
2022-03-06 15:57:12 +0200

    Use TextInputWithFocusButton in App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import TextInputWithFocusButton from './components/TextInputWithFocusButton'
+
 const App = () => {
   return (
     <div>
       <h1>Functional React Refs</h1>
+
+      <h2>TextInputWithFocusButton</h2>
+      <TextInputWithFocusButton />
     </div>
   )
 }
